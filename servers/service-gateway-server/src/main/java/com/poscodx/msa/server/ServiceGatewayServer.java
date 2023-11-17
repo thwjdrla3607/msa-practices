daedec7de8c2fc0581e4421a3f5e0cfae2e85836
sso <sojeong7524@gmail.com>
2023-11-17 09:09:19 +0900

    09

---
+++ servers/service-gateway-server/src/main/java/com/poscodx/msa/server/ServiceGatewayServer.java
@@ -0,0 +1,15 @@
+package com.poscodx.msa.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ServiceGatewayServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceGatewayServer.class, args);
+	}
+
+}
